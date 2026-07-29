f458b43bbf9b28cd867720bc253603eff5016656
liaoxiuqing <45380815+liaoxiuqing@users.noreply.github.com>
2026-07-29 09:30:57 +0800

    update src file from pakeplus

---
+++ src/config.js
@@ -0,0 +1,20 @@
+ var CONFIG ={
+    // 接口域名
+    baseUrl:'https://web.fondcard.net/',
+
+    // 查询设备类型
+    // baseUrl2:'https://www.fondcard.net/',
+    
+    // 图片数据设置
+    //人脸图片路径
+    type_face: '/face/',
+    //人脸模板路径
+    type_temp: "/temp/",
+
+    type_head: '/pic/headee/',
+   
+}
+// var mqtt_api_url =CONFIG.baseUrl+"open/mqtt/api.php?method="
+// module.exports = {
+//     CONFIG
+// }
